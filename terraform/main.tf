8ff2ff831947bc353df69419c69d3e9c57ede21d
temi-daniel <adeoyetemiloluwa54@gmail.com>
2026-08-04 18:28:06 +0100

    CI WORKFLOW

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,236 +1,240 @@
-# VPC AND SUBNET CONFIGURATION
-resource "aws_vpc" "execute" {
-    cidr_block = var.vpc_cidr
-    enable_dns_support = var.vpc_enable_dns_support
-    enable_dns_hostnames = var.vpc_enable_dns_hostnames
-    vpc_security_group_ids = [aws_security_group.execute-sg.id]
+# # VPC AND SUBNET CONFIGURATION
+# resource "aws_vpc" "execute" {
+#     cidr_block = var.vpc_cidr
+#     enable_dns_support = var.vpc_enable_dns_support
+#     enable_dns_hostnames = var.vpc_enable_dns_hostnames
+#     vpc_security_group_ids = [aws_security_group.execute-sg.id]
 
-    tags = {
-        Name = "${var.project_name}-vpc"
-        Environment = var.vpc_environment
-    }
-}
-resource "aws_subnet" "public1" {
-    vpc_id = aws_vpc.execute.id
-    cidr_block = var.public_subnet1_cidr
-    availability_zone = var.az1
-    map_ip_on_lauch = true
-
-
-    tags = {
-        Name = "${var.project_name}-public-subnet-1"
-        Environment = var.vpc_environment
-    }
-}
-
-resource "aws_subnet" "public2" {
-    vpc_id = aws_vpc.execute.id
-    cidr_block = var.public_subnet2_cidr
-    availability_zone = var.az2
-    map_ip_on_lauch = true
-
-    tags = {
-        Name = "${var.project_name}-public-subnet-2"
-        Environment = var.vpc_environment
-    }
-}
-
-resource "aws_subnet" "private1" {
-    vpc_id = aws_vpc.execute.id
-    cidr_block = var.private_subnet1_cidr
-    availability_zone = var.az1
-
-    tags = {
-        Name = "${var.project_name}-private-subnet-1"
-        Environment = var.vpc_environment
-    }
-}
-resource "aws_subnet" "private2" {
-    vpc_id = aws_vpc.execute.id
-    cidr_block = var.private_subnet2_cidr
-    availability_zone = var.az2
-
-    tags = {
-        Name = "${var.project_name}-private-subnet-2"
-        Environment = var.vpc_environment
-    }
-}
-
-# INTERNET GATEWAY
-
-resource "aws_internet_gateway" "main" {
-    vpc_id = aws_vpc.execute.id
-
-    tags = {
-        Name = "${var.project_name}-igw"
-    }
-}
-# ELASTIC IP
-
-resource "aws_eip" "web" {
-    domain = "vpc"
-
-    tags = {
-        Name = "${var.project_name}-web-eip"
-    }
-}
-
-# NAT GATEWAY
-
-resource "aws_nat_gateway" "nat" {
-    allocation_id = aws_eip.web.id
-    subnet_id = aws_subnet.public[1].id
-
-    depends_on = [aws_internet_gateway.nat]
-
-    tags = {
-        Name = "${var.project_name}-nat"
-    }
-}
-
-# PUBLIC ROUTE TABLE
-
-resource "aws_route_table" "public" {
-    vpc_id = aws_vpc.execute.id
-
-    route = {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.main.id
-    }
-
-    tags = {
-        Name = "${var.project_name}-public-route"
-    }
-}
-
-# PRIVATE ROUTE TABLE
-
-resource "aws_route_table" "private" {
-    vpc_id = aws_vpc.execute.id
-
-    route = {
-        cidr_block = "0.0.0.0/0"
-        nat_gateway_id = aws_nat_gateway.nat.id
-    }
-
-    tags = {
-        Name = "${var.project_name}-public-route"
-    }
-}
+#     tags = {
+#         Name = "${var.project_name}-vpc"
+#         Environment = var.vpc_environment
+#     }
+# }
+# resource "aws_subnet" "public1" {
+#     vpc_id = aws_vpc.execute.id
+#     cidr_block = var.public_subnet1_cidr
+#     availability_zone = var.az1
+#     map_ip_on_lauch = true
 
 
-# ECR CONFIGURATION
+#     tags = {
+#         Name = "${var.project_name}-public-subnet-1"
+#         Environment = var.vpc_environment
+#     }
+# }
 
-resource "aws_ecr_repository" "website" {
-    name = var.website_ecr_repository
-    image_tag_mutability = var.ecr_image_tag_mutability
+# resource "aws_subnet" "public2" {
+#     vpc_id = aws_vpc.execute.id
+#     cidr_block = var.public_subnet2_cidr
+#     availability_zone = var.az2
+#     map_ip_on_lauch = true
 
-    image_scanning_configuration {
-      scan_on_push = var.ecr_image_scanning
-    }
-    encryption_configuration {
-      encryption_type = var.ecr_encryption_settings
-    }
+#     tags = {
+#         Name = "${var.project_name}-public-subnet-2"
+#         Environment = var.vpc_environment
+#     }
+# }
 
-    tags = {
-        Name = "${var.project_name}-website_repo"
-        Environment = var.ecr_environment
-    }
-}
+# resource "aws_subnet" "private1" {
+#     vpc_id = aws_vpc.execute.id
+#     cidr_block = var.private_subnet1_cidr
+#     availability_zone = var.az1
 
-resource "aws_ecr_repository" "project" {
-    name = var.project_ecr_repository
-    image_tag_mutability = var.ecr_image_tag_mutability
+#     tags = {
+#         Name = "${var.project_name}-private-subnet-1"
+#         Environment = var.vpc_environment
+#     }
+# }
+# resource "aws_subnet" "private2" {
+#     vpc_id = aws_vpc.execute.id
+#     cidr_block = var.private_subnet2_cidr
+#     availability_zone = var.az2
 
-    image_scanning_configuration {
-      scan_on_push = var.ecr_image_scanning
-    }
+#     tags = {
+#         Name = "${var.project_name}-private-subnet-2"
+#         Environment = var.vpc_environment
+#     }
+# }
 
-    encryption_configuration {
-      encryption_type = var.ecr_encryption_settings
-    }
+# # INTERNET GATEWAY
 
-    tags = {
-        Name = "${var.project_name}-project_repo"
-        Environment = var.ecr_environment
-    }
-}
+# resource "aws_internet_gateway" "main" {
+#     vpc_id = aws_vpc.execute.id
 
-# ECS CONFIGURATION
+#     tags = {
+#         Name = "${var.project_name}-igw"
+#     }
+# }
+# # ELASTIC IP
 
-resource "aws_ecs_cluster" "execute" {
-    cluster = var.ecs_service_name
+# resource "aws_eip" "web" {
+#     domain = "vpc"
 
-}
+#     tags = {
+#         Name = "${var.project_name}-web-eip"
+#     }
+# }
 
-# ECS TASK DEFINITION
+# # NAT GATEWAY
 
-resource "aws_task_definition" "website" {
-    task_definition = var.task_definition_family
-    task_cpu = var.task_cpu
-    requires_compatibilities = ["FARGATE"]
-    network_mode = var.network_mode
-    task_memory = var.task_memory
-    task_role_arn = var.task_role_arn
-    execution_role_arn = var.task_execution_role_arn
+# resource "aws_nat_gateway" "nat" {
+#     allocation_id = aws_eip.web.id
+#     subnet_id = aws_subnet.public[1].id
 
-    container_definitions = jsonencode([
-        {
-            name = var.website_container_name
-            image = var.website_container_image
+#     depends_on = [aws_internet_gateway.nat]
 
-            portMappings = [
-                {
-                    containerport = var.website_container_port
-                    protocol = "tcp"
-                }
-            ]
-        }
-    ])
-}
+#     tags = {
+#         Name = "${var.project_name}-nat"
+#     }
+# }
 
-# ECS SERVICE
+# # PUBLIC ROUTE TABLE
 
-resource "aws_ecs_service" "website" {
-    name = var.ecs_service_name
-    cluster = aws_ecs_cluster.execute.id
-    task_definition = aws_ecs_task_definition.website
-    desired_count = var.desired_count
-    laundh_type = "FARGATE"
+# resource "aws_route_table" "public" {
+#     vpc_id = aws_vpc.execute.id
 
-    network_configuration = {
-        subnet = aws_subnet.private[1].id
+#     route = {
+#         cidr_block = "0.0.0.0/0"
+#         gateway_id = aws_internet_gateway.main.id
+#     }
 
-        security_groups = [
-            aws_security_group.execute-sg.id
-        ]
+#     tags = {
+#         Name = "${var.project_name}-public-route"
+#     }
+# }
 
-        depends_on = [
-            aws_ecs_cluster.execute
-        ]
-    }
-}
-resource "aws_security_group" "execute-sg" {
-    name = var.security_group_ecs
-    description = "security_group_for_ecs"
-    vpc_id = aws_vpc.execute.id
-}
-# RULE FOR SECURITY GROUP
+# # PRIVATE ROUTE TABLE
 
-resource "aws_security_group_rule" "inbound" {
-    security_group_id = aws_security_group.execute-sg.id
-    type = "ingress"
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = var.allowed_ssh_cidr_blocks
-}
+# resource "aws_route_table" "private" {
+#     vpc_id = aws_vpc.execute.id
 
-resource "aws_security_group_rule" "outbound" {
-    security_group_id = aws_security_group.execute-sg.id
-    type = "egress"
-    from_port = 0
-    to_port = 0
-    protocol = -1
-    cidr_blocks = var.allowed_ssh_cidr_blocks
-}
+#     route = {
+#         cidr_block = "0.0.0.0/0"
+#         nat_gateway_id = aws_nat_gateway.nat.id
+#     }
+
+#     tags = {
+#         Name = "${var.project_name}-public-route"
+#     }
+# }
+
+
+# # ECR CONFIGURATION
+
+# resource "aws_ecr_repository" "website" {
+#     name = var.website_ecr_repository
+#     image_tag_mutability = var.ecr_image_tag_mutability
+
+#     image_scanning_configuration {
+#       scan_on_push = var.ecr_image_scanning
+#     }
+#     encryption_configuration {
+#       encryption_type = var.ecr_encryption_settings
+#     }
+
+#     tags = {
+#         Name = "${var.project_name}-website_repo"
+#         Environment = var.ecr_environment
+#     }
+# }
+
+# resource "aws_ecr_repository" "project" {
+#     name = var.project_ecr_repository
+#     image_tag_mutability = var.ecr_image_tag_mutability
+
+#     image_scanning_configuration {
+#       scan_on_push = var.ecr_image_scanning
+#     }
+
+#     encryption_configuration {
+#       encryption_type = var.ecr_encryption_settings
+#     }
+
+#     tags = {
+#         Name = "${var.project_name}-project_repo"
+#         Environment = var.ecr_environment
+#     }
+# }
+
+# # ECS CONFIGURATION
+
+# resource "aws_ecs_cluster" "execute" {
+#     cluster = var.ecs_service_name
+
+# }
+
+# # ECS TASK DEFINITION
+
+# resource "aws_task_definition" "website" {
+#     task_definition = var.task_definition_family
+#     task_cpu = var.task_cpu
+#     requires_compatibilities = ["FARGATE"]
+#     network_mode = var.network_mode
+#     task_memory = var.task_memory
+#     task_role_arn = var.task_role_arn
+#     execution_role_arn = var.task_execution_role_arn
+
+#     container_definitions = jsonencode([
+#         {
+#             name = var.website_container_name
+#             image = var.website_container_image
+
+#             portMappings = [
+#                 {
+#                     containerport = var.website_container_port
+#                     protocol = "tcp"
+#                 }
+#             ]
+#         }
+#     ])
+# }
+
+# # ECS SERVICE
+
+# resource "aws_ecs_service" "website" {
+#     name = var.ecs_service_name
+#     cluster = aws_ecs_cluster.execute.id
+#     task_definition = aws_ecs_task_definition.website
+#     desired_count = var.desired_count
+#     laundh_type = "FARGATE"
+
+#     network_configuration = {
+#         subnet = aws_subnet.private[1].id
+
+#         security_groups = [
+#             aws_security_group.execute-sg.id
+#         ]
+
+#         depends_on = [
+#             aws_ecs_cluster.execute
+#         ]
+#     }
+# }
+# resource "aws_security_group" "execute-sg" {
+#     name = var.security_group_ecs
+#     description = "security_group_for_ecs"
+#     vpc_id = aws_vpc.execute.id
+# }
+# # RULE FOR SECURITY GROUP
+
+# resource "aws_security_group_rule" "inbound" {
+#     security_group_id = aws_security_group.execute-sg.id
+#     type = "ingress"
+#     from_port = 80
+#     to_port = 80
+#     protocol = "tcp"
+#     cidr_blocks = var.allowed_ssh_cidr_blocks
+# }
+
+# resource "aws_security_group_rule" "outbound" {
+#     security_group_id = aws_security_group.execute-sg.id
+#     type = "egress"
+#     from_port = 0
+#     to_port = 0
+#     protocol = -1
+#     cidr_blocks = var.allowed_ssh_cidr_blocks
+# }
+
+# resource "aws_ecs" "name" {
+
+# }
--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,190 +1,122 @@
-# AWS REGION
-variable aws_region {
-    type = string
-    default = "eu-north-1"
-}
-variable project_name {
-    type = string
-    default = "execute-tech"
-}
+# # AWS REGION
+# variable aws_region {
+#     type = string
+#     default = "eu-north-1"
+# }
+# variable project_name {
+#     type = string
+#     default = "execute-tech"
+# }
 
-# VPC CONFIGURATION
-variable vpc_name {
-    type = string
-    default = "execute-tech-vpc"
-}
-variable vpc_cidr {
-    type = string
-    default = "10.0.0.0/16"
-}
-variable vpc_enable_dns_support {
-    type = bool
-    default = true
-}
-variable vpc_enable_dns_hostnames {
-    type = bool
-    default = true
-}
-variable vpc_environment {
-    type = string
-    default = "dev"
-}
-# AVAILABILITY ZONES
-variable az1 {
-    type = string
-    default = "eu-north-1a"
-}
-variable az2 {
-    type = string
-    default = "eu-north-1b"
-}
+# # VPC CONFIGURATION
+# variable vpc_cidr {
+#     type = string
+#     default = "10.0.0.0/16"
+# }
+# variable vpc_enable_dns_support {
+#     type = bool
+#     default = true
+# }
+# variable vpc_enable_dns_hostnames {
+#     type = bool
+#     default = true
+# }
+# variable vpc_environment {
+#     type = string
+#     default = "dev"
+# }
+# # AVAILABILITY ZONES
+# variable az1 {
+#     type = string
+#     default = "eu-north-1a"
+# }
+# variable az2 {
+#     type = string
+#     default = "eu-north-1b"
+# }
 
-#PUBLIC SUBNETS
-variable public_subnet1_cidr {
-    type = string
-    default = "10.0.1.0/24"
-}
+# #PUBLIC SUBNETS
+# variable public_subnet1_cidr {
+#     type = string
+#     default = "10.0.1.0/24"
+# }
 
-variable public_subnet2_cidr {
-    type = string
-    default = "10.0.2.0/24"
-}
-#PRIVATE SUBNETS
+# variable public_subnet2_cidr {
+#     type = string
+#     default = "10.0.2.0/24"
+# }
+# #PRIVATE SUBNETS
 
-variable private_subnet1_cidr {
-    type = string
-    default = "10.0.3.0/24"
-}
+# variable private_subnet1_cidr {
+#     type = string
+#     default = "10.0.3.0/24"
+# }
 
-variable private_subnet2_cidr {
-    type = string
-    default = "10.0.4.0/24"
-}
+# variable private_subnet2_cidr {
+#     type = string
+#     default = "10.0.4.0/24"
+# }
+# variable internet_gateway_id {
+#     type = string
+#     default = "igw"
+# }
 
+# #ECR CONFIGURATION
 
-#ECR CONFIGURATION
+# variable website_ecr_repository {
+#     type = string
+#     default = "execute_tech_website"
+# }
+# variable project_ecr_repository {
+#     type = string
+#     default = "execute_tech_project"
+# }
+# variable ecr_repository_type {
+#     type = string
+#     default = "private"
+# }
+# variable ecr_image_tag_mutability {
+#     type = string
+#     default = "IMMUTABLE"
+# }
+# variable ecr_encryption_settings {
+#     type = string
+#     default = "AES-256"
+# }
+# variable ecr_image_scanning {
+#     type = bool
+#     default = true
+# }
+# variable ecr_environment {
+#     type = string
+#     default = "dev"
+# }
 
-variable website_ecr_repository {
-    type = string
-    default = "execute_tech_website"
-}
-variable project_ecr_repository {
-    type = string
-    default = "execute_tech_project"
-}
-variable ecr_repository_type {
-    type = string
-    default = "private"
-}
-variable ecr_image_tag_mutability {
-    type = string
-    default = "IMMUTABLE"
-}
-variable ecr_encryption_settings {
-    type = string
-    default = "AES-256"
-}
-variable ecr_image_scanning {
-    type = bool
-    default = true
-}
-variable ecr_environment {
-    type = string
-    default = "dev"
-}
+# #ECS CONFIGURATION
 
-#ECS CONFIGURATION
+# variable ecs_cluster_name {
+#     type = string
+#     default = "execute-tech"
+# }
+# variable aws_region {
+#     type = string
+#     default = "eu-north-1"
+# }
 
-variable ecs_cluster_name {
-    type = string
-    default = "execute-tech-cluster"
-}
-variable aws_region {
-    type = string
-    default = "eu-north-1"
-}
+# # TASK DEFINITION
 
-# TASK DEFINITION
-
-variable task_definition_family {
-    type = string
-    default = "execute-tech-task"
-}
-variable task_cpu {
-    type = string
-    default = "2048"
-}
-variable task_memory {
-    type = string
-    default = "4096"
-}
-variable network_mode {
-    type = string
-    default = "awsvpc"
-}
-variable task_role_arn {
-    type = string
-    default = "ecsTaskExecutionRole"
-}
-variable task_execution_role_arn {
-    type = string 
-    default = "ecsTaskExecutionRole"
-}
-variable requires_compatibilities {
-    type = list(string)
-    default = ["FARGATE"]
-}
-
-# CONTAINER
-variable website_container_name {
-    type = string
-    default = "website-container"
-}
-variable project_container_name {
-    type = string
-    default = "project-app-container"
-}
-variable website_container_image {
-    type = string
-    default = "818005122467.dkr.ecr.eu-north-1.amazonaws.com/execute_tech_website" 
-}
-variable website_container_image {
-    type = string
-    default = "818005122467.dkr.ecr.eu-north-1.amazonaws.com/execute_tech_project"
-}
-variable website_container_port {
-    type = number 
-    default = 80
-}
-variable project_container_port {
-    type = number 
-    default = 80
-}
-
-# ECS SERVICE
-variable ecs_service_name {
-    type = string
-    default = "execute-tech-service"
-}
-variable desired_count {
-    type = number 
-    default = 2
-}
-variable assign_public_ip {
-    type = bool
-    default = false
-}
-# NETWORKING
-
-variable security_group_ecs {
-    type = string
-    default = "execute-tech-sg"
-}
-variable allowed_ssh_cidr_blocks {
-    type = list(string)
-
-    validation {
-        condition = !conditions(var.allowed_ssh_cidr_blocks, 0.0.0.0/0)
-    }
-}
-
+# variable task_definition {
+#     type = string
+#     default = "execute-tech-task"
+# }
+# variable task_cpu {
+#     type = string
+#     default = "2048"
+# }
+# variable task_definition_memory {
+#     type = string
+#     default = "4096"
+# }
+# variable task_network_mode {
+#     type = string
+#     default = "awsvpc"
