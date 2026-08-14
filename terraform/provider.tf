60d26e797cfe11878d6789b753c4893697cff5e4
temi-daniel <adeoyetemiloluwa54@gmail.com>
2026-08-14 09:28:54 +0100

    Configure remote S3 backend and CI pipeline

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -5,7 +5,18 @@ terraform {
             version = "6.51.0"
         }
     }
+
+    backend "s3" {
+    bucket = "execute-tech-terraform-state"
+    key = "execute-tech/terraform.tfstate"
+    region = "eu-north-1"
+    encrypt = true
+    use_lockfile = true
+
+    }
+
 }
 provider "aws" {
     region = var.aws_region
 }
+
